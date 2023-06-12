2a98b05b72037c9de8c9b0eb0f9e6a44d7d93f64
efgeorge <enache_george99@yahoo.com>
2023-06-12 16:24:05 +0300

    removed irrelevant buttons

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -2,18 +2,17 @@ import { useState } from 'react';
 import './../styles/QuoteBox.scss';
 import Button from './Button';
 
-
-
 const QuoteBox = ({}) => {
     const quotes = [
         {quote: "Be yourself; everyone else is already taken.", author: "Oscar Wilde"},
         {quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.", author: "Albert Einstein"},
         {quote: "Soarele, atata timp cat sta pe cer, tine de cald la toata lumea.", author: "Anaser Duduianu"}
     ]
+    
 
     const [randomNumber, setRandomNumber] = useState(0);
 
-    const generateRandomNumber = () => {
+    const generateRandomNumberHandler = () => {
         const randomNumber = Math.floor(Math.random() * quotes.length);
         setRandomNumber(randomNumber);
     }
@@ -27,9 +26,7 @@ const QuoteBox = ({}) => {
                 {quotes[randomNumber].author}
             </div>
             <div id="button-wrapper">
-                <Button text={"Twitter"} btnid={"tweet-quote"}/>
-                <Button text={"Tumblr"} btnid={"tumblr-quote"}/>
-                <Button text={"New quote"} btnid={"new-quote"} handler={generateRandomNumber} />
+                <Button text={"New quote"} btnid={"new-quote"} handler={generateRandomNumberHandler} />
             </div>
         </div>
     )
