2c4d29d2ad2f044490f242d5476fde7cf5fe9ec8
efgeorge <fgeorge.enache@gmail.com>
2023-08-30 15:22:59 +0300

    random color generated when onClick

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,24 +1,30 @@
 import { useState } from 'react';
 import './../styles/QuoteBox.scss';
 import Button from './Button';
+import useGenerateRandomColor from '../utils/useGenerateRandomColor';
 
-const QuoteBox = ({}) => {
+const QuoteBox = () => {
     const quotes = [
         {quote: "Be yourself; everyone else is already taken.", author: "Oscar Wilde"},
         {quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.", author: "Albert Einstein"},
         {quote: "Soarele, atata timp cat sta pe cer, tine de cald la toata lumea.", author: "Anaser Duduianu"}
     ]
     
-
     const [randomNumber, setRandomNumber] = useState(Math.floor(Math.random() * quotes.length));
+    const { color, generateColor } = useGenerateRandomColor();
 
     const generateRandomNumberHandler = () => {
         const randomNumber = Math.floor(Math.random() * quotes.length);
         setRandomNumber(randomNumber);
     }
 
+    const handleClick = () => {
+        generateRandomNumberHandler();
+        generateColor();    
+    }
+
     return (
-        <div id="quote-box">
+        <div id="quote-box" style={{backgroundColor: color, color: !color}}>
             <div id="quote-text">
                 {quotes[randomNumber].quote}
             </div>
@@ -26,7 +32,7 @@ const QuoteBox = ({}) => {
                 {quotes[randomNumber].author}
             </div>
             <div id="button-wrapper">
-                <Button text={"New quote"} btnid={"new-quote"} handler={generateRandomNumberHandler} />
+                <Button text={"New quote"} btnid={"new-quote"} handler={handleClick} />
             </div>
         </div>
     )
